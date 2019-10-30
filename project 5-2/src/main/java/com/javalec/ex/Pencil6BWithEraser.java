937a86570d3675170453a82b32f66d4ee7332638
viapolar <56968569+viapolar@users.noreply.github.com>
2019-10-30 17:40:19 +0900

    pencil

---
+++ project 5-2/src/main/java/com/javalec/ex/Pencil6BWithEraser.java
@@ -0,0 +1,12 @@
+package com.javalec.ex;
+
+public class Pencil6BWithEraser extends Pencil6B implements Pencil{
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("6B 굵기로 쓰이고, 지우개가 있습니다.");
+	}
+
+	
+}
